5be3df363e9e1b232dd788171272679ae62a1d52
bhavinmoriya <bhavinmoriya@gmail.com>
2024-07-12 23:32:41 +0200

    modified add function

--- glweAdd.cpp
+++ glweAdd.cpp
@@ -4,7 +4,25 @@
 #include <string>
 using namespace std;
 
-// Function to add two polynomials
+// Function to multiply two polynomials
+std::vector<int> multiplyPolynomials(const std::vector<int> &A, const std::vector<int> &B)
+{
+    int m = A.size();
+    int n = B.size();
+    std::vector<int> result(m + n - 1, 0);
+
+    // Multiply every term of A with every term of B
+    for (int i = 0; i < m; ++i)
+    {
+        for (int j = 0; j < n; ++j)
+        {
+            result[i + j] += A[i] * B[j];
+        }
+    }
+
+    return result;
+}
+
 std::vector<int> addingPolynomials(const std::vector<int> &A, const std::vector<int> &B)
 {
     int m = A.size();
@@ -63,21 +81,39 @@ void printPolynomial(const std::vector<int> &poly, int n)
     }
     std::cout << std::endl;
 }
-
+void deltaM(std::vector<int> &A, int &delta)
+{
+    for (int i = 0; i < A.size(); ++i)
+    {
+        A[i] *= delta;
+    }
+}
 int main()
 {
     // Example polynomials A(x) and B(x)
-    int N = 4;
-    std::vector<int> A = {3, 2, 5}; // 3 + 2x + 5x^2
-    std::vector<int> B = {5, 1, 2}; // 5 + 1x + 2x^2
+    int q = 64, p = 4, k = 2, N = 4;
+    int delta = q / p;
+    // std::vector<int> A = {3, 2, 5}; // 3 + 2x + 5x^2
+    // std::vector<int> B = {5, 1, 2}; // 5 + 1x + 2x^2
+    std::vector<int> A0 = {17, -2, -24, 9}; // 17 + 2x-24x^2+9x^3
+    std::vector<int> A1 = {-14, 0, -1, 21}; //
+    A0 = {-8, 15, 3, -30};
+    A1 = {23, -16, 27, -4};
+    std::vector<int> S0 = {0, 1, 1}; //
+    std::vector<int> S1 = {1, 0, 1, 1};
+    std::vector<int> m1 = {-2, 1, 0, -1};
+    std::vector<int> m2 = {0, 1, 1, -2};
+    std::vector<int> e1 = {-1, 1, 0, 1};
+    std::vector<int> e2 = {0, 1, -1, -1};
 
-    std::cout << "Polynomial A: ";
-    printPolynomial(A, A.size());
-    std::cout << "Polynomial B: ";
-    printPolynomial(B, B.size());
+    std::vector<int> result = multiplyPolynomials(A0, S0);
+    result = addingPolynomials(result, multiplyPolynomials(A1, S1));
+    deltaM(m2, delta);
+    printPolynomial(e1, e1.size());
+    result = addingPolynomials(result, m2);
+    result = addingPolynomials(result, e2);
 
-    std::vector<int> result = addingPolynomials(A, B);
-
+    // printPolynomial(result, result.size());
     std::cout << "Resultant Polynomial: ";
     printPolynomial(result, result.size());
     std::cout << string(26, '*') << endl;
